7f2c118ffdd265ade4479194121dcaaaf6a9d57a
mk124 <mk124a@gmail.com>
2012-12-12 21:37:49 +0800

    Add more tests.

--- src/test/java/net/gtaun/util/event/ManagedEventManagerTest.java
+++ src/test/java/net/gtaun/util/event/ManagedEventManagerTest.java
@@ -2,15 +2,10 @@ package net.gtaun.util.event;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
-
-import java.util.LinkedList;
-import java.util.Queue;
 
 import net.gtaun.util.event.EventManager.HandlerEntry;
 import net.gtaun.util.event.EventManager.HandlerPriority;
-import net.gtaun.util.event.EventManager.ThrowableHandler;
 import net.gtaun.util.event.events.InterruptableEvent;
 import net.gtaun.util.event.events.UselessEvent;
 import net.gtaun.util.event.events.UselessEventHandler;
@@ -19,30 +14,33 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class EventManagerTest
+public class ManagedEventManagerTest
 {
-	private EventManager eventManager;
+	private EventManager rootEventManager;
+	private ManagedEventManager eventManager;
 	
 	
-	public EventManagerTest()
+	public ManagedEventManagerTest()
 	{
 
 	}
-	
+
 	@Before
 	public void setUp() throws Exception
 	{
-		eventManager = new EventManagerImpl();
+		rootEventManager = new EventManagerImpl();
+		eventManager = new ManagedEventManager(rootEventManager);
 	}
-	
+
 	@After
 	public void tearDown() throws Exception
 	{
+		rootEventManager = null;
 		eventManager = null;
 	}
 	
 	@Test
-	public void testAddHandlerAndDispatchEvent()
+	public void testRegisterHandlerAndDispatchEvent()
 	{
 		UselessEventHandler handler = new UselessEventHandler()
 		{
@@ -56,13 +54,13 @@ public class EventManagerTest
 		eventManager.registerHandler(UselessEvent.class, handler, HandlerPriority.NORMAL);
 		
 		UselessEvent event = new UselessEvent();
-		eventManager.dispatchEvent(event);
+		rootEventManager.dispatchEvent(event);
 		
 		assertTrue(event.isProcessed());
 	}
-	
+
 	@Test
-	public void testAddHandlerAndDispatchEvent2()
+	public void testRegisterHandlerAndDispatchEvent2()
 	{
 		final TapableCounter counter = new TapableCounter();
 		
@@ -78,15 +76,15 @@ public class EventManagerTest
 		eventManager.registerHandler(InterruptableEvent.class, this, handler, HandlerPriority.NORMAL);
 		
 		InterruptableEvent event = new InterruptableEvent();
-		eventManager.dispatchEvent(event, this);
-		eventManager.dispatchEvent(event, new Object());
-		eventManager.dispatchEvent(event);
+		rootEventManager.dispatchEvent(event, this);
+		rootEventManager.dispatchEvent(event, new Object());
+		rootEventManager.dispatchEvent(event);
 		
 		assertEquals(1, counter.getTaps());
 	}
-	
+
 	@Test
-	public void testAddHandlerAndDispatchEvent3()
+	public void testRegisterHandlerAndDispatchEvent3()
 	{
 		final TapableCounter counter = new TapableCounter();
 		
@@ -99,18 +97,18 @@ public class EventManagerTest
 			}
 		};
 		
-		eventManager.registerHandler(InterruptableEvent.class, EventManagerTest.class, handler, HandlerPriority.NORMAL);
+		eventManager.registerHandler(InterruptableEvent.class, getClass(), handler, HandlerPriority.NORMAL);
 		
 		InterruptableEvent event = new InterruptableEvent();
-		eventManager.dispatchEvent(event, this);
-		eventManager.dispatchEvent(event, new Object());
-		eventManager.dispatchEvent(event);
+		rootEventManager.dispatchEvent(event, this);
+		rootEventManager.dispatchEvent(event, new Object());
+		rootEventManager.dispatchEvent(event);
 		
 		assertEquals(1, counter.getTaps());
 	}
 	
 	@Test
-	public void testAddHandlerAndDispatchEvent4()
+	public void testRegisterHandlerAndDispatchEvent4()
 	{
 		final TapableCounter counter = new TapableCounter();
 		
@@ -130,15 +128,15 @@ public class EventManagerTest
 		eventManager.registerHandler(InterruptableEvent.class, Cloneable.class, handler, HandlerPriority.NORMAL);
 		
 		InterruptableEvent event = new InterruptableEvent();
-		eventManager.dispatchEvent(event, cloneable);
-		eventManager.dispatchEvent(event, new Object());
-		eventManager.dispatchEvent(event);
+		rootEventManager.dispatchEvent(event, cloneable);
+		rootEventManager.dispatchEvent(event, new Object());
+		rootEventManager.dispatchEvent(event);
 		
 		assertEquals(1, counter.getTaps());
 	}
 	
 	@Test
-	public void testRemoveHandler()
+	public void testCancelHandler()
 	{
 		UselessEventHandler handler = new UselessEventHandler()
 		{
@@ -153,89 +151,71 @@ public class EventManagerTest
 		entry.cancel();
 		
 		UselessEvent event = new UselessEvent();
-		eventManager.dispatchEvent(event);
+		rootEventManager.dispatchEvent(event);
 		
 		assertFalse(event.isProcessed());
 	}
 	
 	@Test
-	public void testInterruptEvent()
+	public void testCancelAllHandler()
 	{
-		final TapableCounter counter = new TapableCounter();
-		
 		UselessEventHandler handler = new UselessEventHandler()
 		{
 			@Override
-			public void onInterruptableEvent(InterruptableEvent event)
+			public void onUselessEvent(UselessEvent event)
 			{
-				counter.tap();
-				event.interrupt();
-			}
-		};
-		
-		UselessEventHandler handler2 = new UselessEventHandler()
-		{
-			@Override
-			public void onInterruptableEvent(InterruptableEvent event)
-			{
-				counter.tap();
-			}
-		};
-
-		eventManager.registerHandler(InterruptableEvent.class, handler, HandlerPriority.NORMAL);
-		eventManager.registerHandler(InterruptableEvent.class, handler2, HandlerPriority.NORMAL);
-		
-		InterruptableEvent event = new InterruptableEvent();
-		eventManager.dispatchEvent(event);
-		
-		assertTrue(counter.getTaps() == 1);
-	}
-	
-	@Test
-	public void testCatchException()
-	{
-		final Exception exception = new Exception();
-		final Queue<Throwable> queue = new LinkedList<Throwable>();
-		
-		UselessEventHandler handler = new UselessEventHandler()
-		{
-			@Override
-			public void onUselessEvent(UselessEvent event) throws Exception
-			{
-				throw exception;
-			}
-		};
-		
-		ThrowableHandler throwableHandler = new ThrowableHandler()
-		{
-			@Override
-			public void handleThrowable(Throwable throwable)
-			{
-				queue.offer(throwable);
+				event.setProcessed(true);
 			}
 		};
 		
 		eventManager.registerHandler(UselessEvent.class, handler, HandlerPriority.NORMAL);
-		eventManager.dispatchEvent(throwableHandler, new UselessEvent());
+		eventManager.cancelAll();
 		
-		assertSame(exception, queue.poll());
+		UselessEvent event = new UselessEvent();
+		rootEventManager.dispatchEvent(event);
+		
+		assertFalse(event.isProcessed());
 	}
 	
 	@Test
-	public void testManagedEventManagerEntryProperties()
+	public void testGcCancelAllHandler() throws InterruptedException
 	{
 		UselessEventHandler handler = new UselessEventHandler()
 		{
+			@Override
+			public void onUselessEvent(UselessEvent event)
+			{
+				event.setProcessed(true);
+			}
 		};
 		
-		ManagedEventManager managedEventManager = new ManagedEventManager(eventManager);
-		HandlerEntry entry = managedEventManager.registerHandler(UselessEvent.class, EntryTest.class, handler, HandlerPriority.LOWEST);
-
-		assertSame(managedEventManager, entry.getEventManager());
-		assertSame(UselessEvent.class, entry.getType());
-		assertSame(EntryTest.class, entry.getRelatedObject());
-		assertSame(EntryTest.class, entry.getRelatedClass());
-		assertSame(handler, entry.getHandler());
-		assertEquals(HandlerPriority.LOWEST.getValue(), entry.getPriority());
+		final Object waitObject = new Object();
+		ManagedEventManager eventManager = new ManagedEventManager(rootEventManager)
+		{
+			@Override
+			protected void finalize() throws Throwable
+			{
+				super.finalize();
+				synchronized (waitObject)
+				{
+					waitObject.notify();
+				}
+			}
+		};
+		
+		eventManager.registerHandler(UselessEvent.class, handler, HandlerPriority.NORMAL);
+		eventManager = null;
+		
+		System.gc();
+		
+		synchronized (waitObject)
+		{
+			waitObject.wait();
+		}
+		
+		UselessEvent event = new UselessEvent();
+		rootEventManager.dispatchEvent(event);
+		
+		assertFalse(event.isProcessed());
 	}
 }
